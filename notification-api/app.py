074f578c7694fe7f6827ce76a967e2e0919aa173
Yash <yashrathod3051999@gmail.com>
2026-04-24 23:50:01 +0530

    chore: trigger initial CI build

--- notification-api/app.py
+++ notification-api/app.py
@@ -19,4 +19,5 @@ def create_app():
     return app
 
 if __name__ == "__main__":
-    create_app().run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    create_app().run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080))) 
+    
